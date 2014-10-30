a3f2e79249a48270b51942c383875510c9c145df
Laud Bentil <laud.bentil@meltwater.org>
2014-10-30 02:53:38 +0000

    added validation to product & review models

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,3 @@
 class Product < ActiveRecord::Base
+	validates :name, presence: true, length: { minimum: 2, maximum: 40}
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,4 @@
 class Review < ActiveRecord::Base
+  validates :comment, presence: true, length: { minimum: 3, maximum: 200}
   belongs_to :product
 end
